8837274b87638cd3b492384af7ab8daf8c12ae4d
jannisborn <jab@zurich.ibm.com>
2022-12-31 12:41:03 +0100

    feat: add functional group extractor

---
+++ terminator/functional_groups.py
@@ -0,0 +1,83 @@
+#
+#  Original authors: Richard Hall and Guillaume Godin
+#  This file is part of the RDKit.
+#  The contents are covered by the terms of the BSD license
+#  which is included in the file license.txt, found at the root
+#  of the RDKit source tree.
+
+#
+#
+# Richard hall 2017
+# IFG main code
+# Guillaume Godin 2017
+# refine output function
+# astex_ifg: identify functional groups a la Ertl, J. Cheminform (2017) 9:36
+from rdkit import Chem
+from collections import namedtuple
+
+
+def merge(mol, marked, aset):
+    bset = set()
+    for idx in aset:
+        atom = mol.GetAtomWithIdx(idx)
+        for nbr in atom.GetNeighbors():
+            jdx = nbr.GetIdx()
+            if jdx in marked:
+                marked.remove(jdx)
+                bset.add(jdx)
+    if not bset:
+        return
+    merge(mol, marked, bset)
+    aset.update(bset)
+
+
+# atoms connected by non-aromatic double or triple bond to any heteroatom
+# c=O should not match (see fig1, box 15).  I think using A instead of * should sort that out?
+PATT_DOUBLE_TRIPLE = Chem.MolFromSmarts("A=,#[!#6]")
+# atoms in non aromatic carbon-carbon double or triple bonds
+PATT_CC_DOUBLE_TRIPLE = Chem.MolFromSmarts("C=,#C")
+# acetal carbons, i.e. sp3 carbons connected to tow or more oxygens, nitrogens or sulfurs; these O, N or S atoms must have only single bonds
+PATT_ACETAL = Chem.MolFromSmarts("[CX4](-[O,N,S])-[O,N,S]")
+# all atoms in oxirane, aziridine and thiirane rings
+PATT_OXIRANE_ETC = Chem.MolFromSmarts("[O,N,S]1CC1")
+
+PATT_TUPLE = (PATT_DOUBLE_TRIPLE, PATT_CC_DOUBLE_TRIPLE, PATT_ACETAL, PATT_OXIRANE_ETC)
+
+
+def identify_functional_groups(mol):
+    marked = set()
+    # mark all heteroatoms in a molecule, including halogens
+    for atom in mol.GetAtoms():
+        if atom.GetAtomicNum() not in (6, 1):  # would we ever have hydrogen?
+            marked.add(atom.GetIdx())
+
+    # mark the four specific types of carbon atom
+    for patt in PATT_TUPLE:
+        for path in mol.GetSubstructMatches(patt):
+            for atomindex in path:
+                marked.add(atomindex)
+
+    # merge all connected marked atoms to a single FG
+    groups = []
+    while marked:
+        grp = set([marked.pop()])
+        merge(mol, marked, grp)
+        groups.append(grp)
+
+    # extract also connected unmarked carbon atoms
+    ifg = namedtuple("IFG", ["atomIds", "atoms", "type"])
+    ifgs = []
+    for g in groups:
+        uca = set()
+        for atomidx in g:
+            for n in mol.GetAtomWithIdx(atomidx).GetNeighbors():
+                if n.GetAtomicNum() == 6:
+                    uca.add(n.GetIdx())
+        ifgs.append(
+            ifg(
+                atomIds=tuple(list(g)),
+                atoms=Chem.MolFragmentToSmiles(mol, g, canonical=True),
+                type=Chem.MolFragmentToSmiles(mol, g.union(uca), canonical=True),
+            )
+        )
+    return ifgs
