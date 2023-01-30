0c4408ca63384f4c9626231661fcdfacbca43e8d
jannisborn <jab@zurich.ibm.com>
2023-01-30 10:21:22 +0100

    wip: selfies requirement formatting

--- setup.py
+++ setup.py
@@ -21,5 +21,11 @@ setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     package_data={"terminator": ["py.typed"]},
-    install_requires=["transformers", "numpy", "tqdm", "selfies>=0.2.4<=1.0.0", "modlamp"],
+    install_requires=[
+        "transformers",
+        "numpy",
+        "tqdm",
+        "selfies>=0.2.4,<=1.0.0",
+        "modlamp",
+    ],
 )
